6bd5c0f4f965a3d5e4aa8ee6223ba39bd2d90e01
7empes7s <125469033+7empes7s@users.noreply.github.com>
2023-04-23 21:52:37 +0000

    Update main.h

--- main.h
+++ main.h
@@ -7,17 +7,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list args, char *buffer, int *j);
-int print_str(va_list args, char *buffer, int *j);
-int print_percent(char *buffer, int *j);
-int print_int(va_list args, char *buffer, int *j);
-int print_unsigned_int(va_list args, char *buffer, int *j);
-int print_binary(va_list args, char *buffer, int *j);
-int print_octal(va_list args, char *buffer, int *j);
-int print_hex(va_list args, char *buffer, int *j, int uppercase);
-int print_address(va_list args, char *buffer, int *j);
-int print_reversed(va_list args, char *buffer, int *j);
-int print_rot13(va_list args, char *buffer, int *j);
 
 typedef struct format
 {
