1980ac60dbdb6889bed5019f43f4c6e7dea4c41e
7empes7s <125469033+7empes7s@users.noreply.github.com>
2023-04-23 22:10:39 +0000

    Update customprintf.c

--- customprintf.c
+++ customprintf.c
@@ -1,134 +1,61 @@
-#include "main.h"
 #include <stdarg.h>
-#include <stddef.h>
-#include <limits.h>
+#include <unistd.h>
 
+/**
+ * _putchar - writes a character to stdout
+ * @c: character to write
+ * Return: 1 on success, -1 on failure
+ */
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    char buffer[1024] = {0};
-    size_t i;
-    int j = 0;
-    format_t formats[] = {
-        {'c', print_char},
-        {'s', print_str},
-        {'%', print_percent},
-        {'d', print_int},
-        {'i', print_int},
-        {'b', print_binary},
-        {'u', print_unsigned},
-        {'o', print_octal},
-        {'x', print_hex},
-        {'X', print_hex_upper},
-        {'p', print_address},
-        {'r', print_reversed},
-        {'R', print_rot13},
-        {0, NULL}};
+	va_list args;
+	int i, count = 0;
 
-    va_start(args, format);
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            int k = 0;
-            int left_padding = 0;
-            int right_padding = 0;
-            int precision = -1;
-            int is_short = 0;
-            int is_long = 0;
+	va_start(args, format);
 
-            if (format[i] == '-')
-            {
-                i++;
-                left_padding = 1;
-            }
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++; /* skip the conversion specifier */
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					count += write(1, va_arg(args, char *), strlen(va_arg(args, char *)));
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
 
-            if (format[i] == '0')
-            {
-                i++;
-                right_padding = 1;
-            }
+	va_end(args);
 
-            while (format[i] >= '0' && format[i] <= '9')
-            {
-                if (precision == -1 && !left_padding)
-                    right_padding = 1;
-                precision = (precision * 10) + (format[i] - '0');
-                i++;
-            }
-
-            if (format[i] == 'h')
-            {
-                is_short = 1;
-                i++;
-            }
-
-            if (format[i] == 'l')
-            {
-                is_long = 1;
-                i++;
-            }
-
-            while (formats[k].type != 0)
-            {
-                if (formats[k].type == format[i])
-                {
-                    formats[k].func(args, buffer, &j, left_padding, right_padding, precision, is_short, is_long);
-                    break;
-                }
-                k++;
-            }
-            if (formats[k].type == 0)
-            {
-                buffer[j++] = format[i - 1];
-                buffer[j++] = format[i];
-            }
-        }
-        else
-            buffer[j++] = format[i];
-    }
-    va_end(args);
-
-    return write(1, buffer, j);
+	return (count);
 }
-
-// Print Functions
-
-int print_char(va_list args, char *buffer, int *j, int left_padding, int right_padding, int precision, int is_short, int is_long)
-{
-    char c = (char)va_arg(args, int);
-    int padding_amount = 0;
-
-    if (left_padding)
-    {
-        buffer[*j] = c;
-        (*j)++;
-    }
-
-    if (precision != -1)
-        padding_amount = precision - 1;
-
-    while (padding_amount > 0)
-    {
-        buffer[*j] = ' ';
-        (*j)++;
-        padding_amount--;
-    }
-
-    if (!left_padding)
-    {
-        buffer[*j] = c;
-        (*j)++;
-    }
-
-    return (1);
-}
-
-int print_str(va_list args, char *buffer, int *j, int left_padding, int right_padding, int precision, int is_short, int is_long)
-{
